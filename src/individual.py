cfe81ad4553ec7c00351e4029fa436e89eda4fd7
dragonmushu <akshay.ben.n@gmail.com>
2019-06-26 19:43:24 -0400

    Added averaging feature to individual

--- src/individual.py
+++ src/individual.py
@@ -13,15 +13,22 @@ class Individual:
         self.parent1 = p1
         self.parent2 = p2
 
-    def add_metric(self, key, val):
-        self.metrics[key] = val
+    def add_metric(self, key, val, average=False):
+        if key not in self.metrics or not average:
+            self.metrics[key] = (val, 1)
+        else:
+            metric = self.metrics[key]
+            old_avg = metric[0]
+            old_size = metric[1]
+            avg = (old_avg * old_size + val)/(old_size + 1)
+            self.metrics[key] = (avg, old_size + 1)
 
     def get_metric(self, key):
-        return self.metrics[key]
+        return self.metrics[key][0]
 
     def gene_active(self, position):
         return utils.bit_set(self.chromosome, position)
 
-    def __clear_metrics__(self):
+    def clear_metrics(self):
         self.metrics = {}
 
---
+++ tests/individual_test.py
@@ -0,0 +1,40 @@
+import unittest
+from src.individual import Individual
+
+
+class TestIndividual(unittest.TestCase):
+
+    def setUp(self):
+        self.individual = Individual()
+
+    def test_add_metric_value(self):
+        key = 'metric'
+        val = 5
+        self.individual.add_metric(key, val)
+        self.assertEqual(1, len(self.individual.metrics))
+        self.assertEqual(5, self.individual.get_metric(key))
+
+    def test_add_metric_overrides(self):
+        key = 'metric'
+        val_1 = 5
+        val_2 = 3
+        self.individual.add_metric(key, val_1)
+        self.individual.add_metric(key, val_2)
+        self.assertEqual(1, len(self.individual.metrics))
+        self.assertEqual(3, self.individual.get_metric(key))
+
+    def test_add_metric_averaging(self):
+        key = 'metric'
+        val_1 = 5
+        val_2 = 3
+        self.individual.add_metric(key, val_1, average=True)
+        self.individual.add_metric(key, val_2, average=True)
+        self.assertEqual(1, len(self.individual.metrics))
+        self.assertEqual(4, self.individual.get_metric(key))
+
+    def tearDown(self):
+        self.individual.clear_metrics()
+
+
+if __name__ == '__main__':
+    unittest.main()
